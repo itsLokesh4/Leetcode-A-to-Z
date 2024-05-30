42ac5f9e6099363b8a098e9ae51dbed093cb867f
LOKESHWARAN <lokilokesh7706@gmail.com>
2024-05-30 20:37:33 +0530

    Amazon-LeetCode Question

---
+++ 371.js
@@ -0,0 +1,24 @@
+// 371. Sum of Two Integers
+
+
+// Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+// Example 1:
+
+// Input: a = 1, b = 2
+// Output: 3
+// Example 2:
+
+// Input: a = 2, b = 3
+// Output: 5
+
+
+
+
+var getSum = function(a, b) {
+
+    let sum = 0
+    sum = a + b
+    return sum
+    
+};
